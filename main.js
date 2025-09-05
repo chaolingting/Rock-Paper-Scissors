78491ea79d7e937cff36628706bf8e0491802ad0
chaolingting <10724352+chaolingting@users.noreply.github.com>
2025-09-05 17:28:51 +0800

    revisit main.js

--- main.js
+++ main.js
@@ -8,11 +8,11 @@ function getComputerChoice(){
  } else if (computerChoice === 1){
     return 'paper';
  } else if (computerChoice === 2){
-    return 'scissor'
+    return 'scissors'
  }
  
 }
-//console.log(getComputerChoice());
+
 
 //Human Choice
 function getHumanChoice(){
@@ -22,69 +22,57 @@ function getHumanChoice(){
 }
 
 
+function playRound(humanChoice,computerChoice){
+        if (humanChoice === 'rock' && computerChoice === 'paper'){
+            console.log('You lose, try again!');
+            computerScore += 1 ;
+            } else if (humanChoice ==='rock' && computerChoice === 'rock' ){
+            console.log('TIE, try again!');
+
+            } else if (humanChoice === 'rock' && computerChoice === 'scissors'){
+                console.log('You win!')
+                humanScore += 1;
+        }
+
+        if (humanChoice ==='paper' && computerChoice === 'rock'){
+            console.log('You win!');
+            humanScore += 1;
+        }else if(humanChoice === 'paper' && computerChoice === 'paper'){
+                console.log('TIE, try again!');
+        }else if (humanChoice === 'paper' && computerChoice === 'scissors'){
+                console.log('You lose, try again');
+                computerScore += 1;
+        }
+
+        if(humanChoice === 'scissors' && computerChoice === 'paper'){
+            console.log('You win!');
+            humanScore += 1;
+        } else if (humanChoice === 'scissors' && computerChoice === 'scissors'){
+            console.log('TIE, try again');
+        } else if (humanChoice === 'scissors' && computerChoice ==='rock'){
+            console.log('You lose, try again');
+            computerScore += 1;
+        }
+
+
+        console.log(`Score: You ${humanScore} : Computer ${computerScore}`);
+
+}
 
 function playGame(){
     
-//Score
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice,computerChoice){
-if (humanChoice === 'rock' && computerChoice === 'paper'){
-console.log('You lose, try again!');
-computerScore += 1 ;
-} else if (humanChoice ==='rock' && computerChoice === 'rock' ){
- console.log('TIE, try again!');
-
-} else if (humanChoice === 'rock' && computerChoice === 'scissor'){
-    console.log('You win!')
-    humanScore += 1;
-}
-
-if (humanChoice ==='paper' && computerChoice === 'rock'){
-    console.log('You win!');
-    humanScore += 1;
-}else if(humanChoice === 'paper' && computerChoice === 'paper'){
-     console.log('TIE, try again!');
-}else if (humanChoice === 'paper' && computerChoice === 'scissor'){
-    console.log('You lose, try again');
-    computerScore += 1;
-}
-
-if(humanChoice === 'scissor' && computerChoice === 'paper'){
-    console.log('You win!');
-    humanScore += 1;
-} else if (humanChoice === 'scissor' && computerChoice === 'scissor'){
-    console.log('TIE, try again');
-} else if (humanChoice === 'scissor' && computerChoice ==='rock'){
-    console.log('You lose, try again');
-    computerScore += 1;
-}
+    //Score
+    let humanScore = 0;
+    let computerScore = 0;
 
 
-console.log(`Score: You ${humanScore} : Computer ${computerScore}`);
-
-
-
-
-
-}
-
-for (let i = 1; i <= 5; i++){
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-console.log(`you choose: ${humanChoice}`);
-console.log(`computer choose: ${computerChoice}`);
-playRound(humanChoice, computerChoice);
-}
-
-if(humanScore > computerScore){
-    console.log('Congrats you win the game! Refresh to play again.');
-}else if(humanScore < computerScore){
-    console.log('computer wins the game, refresh to play again!');
-} else{
-    console.log('This game is a tie, refresh to play agian!');
-}
+    if(humanScore > computerScore){
+            console.log('Congrats you win the game! Refresh to play again.');
+        }else if(humanScore < computerScore){
+            console.log('computer wins the game, refresh to play again!');
+        } else{
+            console.log('This game is a tie, refresh to play agian!');
+    }
 
 }
 
